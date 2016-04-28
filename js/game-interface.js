e6a76a24ff0513290c9d8f33e75e49224d60b5f6
Minh Ho <hodangminh@gmail.com>
2016-04-28 16:08:09 -0700

    checking results

--- js/game-interface.js
+++ js/game-interface.js
@@ -1,3 +1,26 @@
+var AnswerFrame = React.createClass({displayName: "AnswerFrame",
+    render:function(){
+        // get the selected number from Game component and render here
+        var selectNumbers = this.props.selectNumbers;
+        var unselectClick = this.props.unSelectNumberClick;
+        var numbers = selectNumbers.map(function(value){
+            return (
+                React.createElement("span", {className: "number", onClick: unselectClick.bind(null,value)}, 
+                    value
+                )
+            )
+        });
+
+        return (
+            React.createElement("div", {id: "answer-frame"}, 
+                React.createElement("div", {className: "well"}, 
+                    numbers
+                )
+            )
+        );
+    }
+});
+
 var StarsFrame = React.createClass({displayName: "StarsFrame",
     getInitialState: function(){
         // return the star value from Game component
@@ -24,34 +47,20 @@ var StarsFrame = React.createClass({displayName: "StarsFrame",
 
 var ButtonFrame = React.createClass({displayName: "ButtonFrame",
     render:function(){
-        var disabled;
-        disabled = (this.props.selectNumbers.length === 0) ? "disabled": "";
+        var disabled = (this.props.selectNumbers.length === 0) ? "disabled": "";
+        var checkResultClick = this.props.checkResultClick;
+        var className = "btn btn-primary btn-lg";
+        var status = this.props.status;
+        if(disabled == "")
+        {
+            if(status != null )
+            {
+                className += (status ?" btn-correct":" btn-incorrect");
+            }
+        }
         return (
             React.createElement("div", {id: "button-frame"}, 
-                React.createElement("button", {className: "btn btn-primary btn-lg", disabled: disabled}, "=")
-            )
-        );
-    }
-});
-
-var AnswerFrame = React.createClass({displayName: "AnswerFrame",
-    render:function(){
-        // get the selected number from Game component and render here
-        var selectNumbers = this.props.selectNumbers;
-        var unselectClick = this.props.unSelectNumberClick;
-        var numbers = selectNumbers.map(function(value){
-            return (
-                React.createElement("span", {className: "number", onClick: unselectClick.bind(null,value)}, 
-                    value
-                )
-            )
-        });
-
-        return (
-            React.createElement("div", {id: "answer-frame"}, 
-                React.createElement("div", {className: "well"}, 
-                    numbers
-                )
+                React.createElement("button", {className: className, disabled: disabled, onClick: checkResultClick}, "=")
             )
         );
     }
@@ -59,21 +68,26 @@ var AnswerFrame = React.createClass({displayName: "AnswerFrame",
 
 var NumberFrame = React.createClass({displayName: "NumberFrame",
     updateParentState: function(value){
-        // this is the NumberFrame, we need to get the value of the click numbers
-
-        var clickNumber = this.props.val;
-        this.props.selectNumberClick(clickNumber);
+        // prevent used number to be clicked.
+        if(this.props.usedNumbers.indexOf(value) < 0)
+        {
+            // this is the NumberFrame, we need to get the value of the click numbers
+            var clickNumber = this.props.val;
+            this.props.selectNumberClick(clickNumber);
+        }
     },
     render:function(){
         var numbers = [];
         var selectedNumbersArray = this.props.selectNumbers;
         // get the update parent click function from props
         var clickNumFunc = this.props.selectNumberClick;
-
+        var usedNumbers = this.props.usedNumbers;
         // NOTE: clickNumFunc.bind(null,i) : create a closure and bind the value to the function.
 
         for(var i = 1; i<=9 ;i++){
             var classAttrs = 'number select-' + ((selectedNumbersArray.indexOf(i) > -1) ? "true" : "false");
+            classAttrs += (usedNumbers.indexOf(i) > -1 ? " number-used": "");
+
             numbers.push(
                 React.createElement("div", {className: classAttrs, onClick: clickNumFunc.bind(null,i)}, i)
             );
@@ -93,11 +107,17 @@ var Game = React.createClass({displayName: "Game",
         return Math.floor( Math.random() * 9) + 1;
     },
     getInitialState: function(){
-        var randomNumber = this.getRandomValue();
         return {
-                numberOfStars: randomNumber,
-                selectedNumbers : []
+                numberOfStars: this.getRandomValue(),
+                selectedNumbers : [],
+                usedNumbers : [],
+                correct: null
             };
+    },
+    redraw: function(){
+        this.replaceState({
+
+        });
     },
     selectNumber : function(value){
         var selectedNumbers = this.state.selectedNumbers;
@@ -113,13 +133,37 @@ var Game = React.createClass({displayName: "Game",
         var selectedNumbers = this.state.selectedNumbers;
         var valueIndex = selectedNumbers.indexOf(value);
 
+        // NOTE: There is an issue when use the line below in the setState. The array did not get removed correctly
+        //       The correct way seems to be run this function before use setState
+        selectedNumbers.splice(valueIndex,1);
+
         if(value > -1)
         {
             // remove the value from the selected number state array
             this.setState(
-                {selectedNumbers: selectedNumbers.splice(valueIndex,1)}
+                {selectedNumbers: selectedNumbers}
             );
         }
+    },
+    checkResult: function(){
+        var numberOfStars = this.state.numberOfStars;
+        var selectedNumbers = this.state.selectedNumbers;
+        var usedNumbers = this.state.usedNumbers;
+        var sum = 0;
+        for(var i = 0 ; i < selectedNumbers.length; i++){
+            sum += selectedNumbers[i];
+        }
+
+        var status = (sum == numberOfStars) ? true : false;
+        // if it is correct add the numbers into the usedNumbers array
+        if(status){
+            usedNumbers = usedNumbers.concat(selectedNumbers);
+        }
+
+        this.setState({
+            correct: status,
+            usedNumbers: usedNumbers
+        })
     },
     render:function(){
         return (
@@ -128,10 +172,10 @@ var Game = React.createClass({displayName: "Game",
                 React.createElement("hr", null), 
                 React.createElement("div", {className: "clearfix"}, 
                     React.createElement(StarsFrame, {numberOfStars: this.state.numberOfStars}), 
-                    React.createElement(ButtonFrame, {selectNumbers: this.state.selectedNumbers}), 
+                    React.createElement(ButtonFrame, {selectNumbers: this.state.selectedNumbers, status: this.state.correct, checkResultClick: this.checkResult}), 
                     React.createElement(AnswerFrame, {selectNumbers: this.state.selectedNumbers, unSelectNumberClick: this.unSelectNumber})
                 ), 
-                React.createElement(NumberFrame, {selectNumbers: this.state.selectedNumbers, selectNumberClick: this.selectNumber})
+                React.createElement(NumberFrame, {selectNumbers: this.state.selectedNumbers, usedNumbers: this.state.usedNumbers, selectNumberClick: this.selectNumber})
             )
         );
     }
@@ -140,3 +184,8 @@ var Game = React.createClass({displayName: "Game",
 React.render(
     React.createElement(Game, null), document.getElementById('container')
 );
+
+// TODO:
+//      Check if the answer is correct
+//      Check if the is no possible answer
+//      Redraw button
